17b2caf7273298988cced391fb6824a47b85d794
IAmGregGarcia <S00853585@acad.tri-c.edu>
2016-06-28 00:02:49 -0400

    fixes manifest conflicts

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
-
+// changes results color to red
 function changeResultColor() {
     var resultCount = document.getElementsByClassName('result-count');
     resultCount[0].style.color = "red"; 
-    // main();
 }
 
 
@@ -13,7 +12,7 @@ var exceptions = {};
     exceptions["Christine Ann Kiel"] = "Chris Kiel";
 
 
-createRMPHeader = function() {   
+function main() {   
 
     changeResultColor();
 
@@ -259,143 +258,16 @@ function addContentToPopUp(popup, profURL, responseText) {
     popup.appendChild(numRatingsDiv);
 }
 
-getFakeProfRating = function(professor) {
-    if(professor === "TBA") {
-        rating = "No professor(s) selected";
-        return rating;
-    } else {
-        rating = "3.5";
-        return rating;
-    }
-}
-
-getProfessorRating = function(professorName) {
-    if(professorName == null) {
-            professorName = "Could not locate professor on webpage";
-        } else {
-
-            // check for middle name
-            var profSplit = professorName.split(" ");   
-
-            if (profSplit.length == 3) {
-                var profArray = swapArrayElements(profSplit, 0, 2);
-            } else {
-                var profArray = swapArrayElements(profSplit, 0, 1);
-            }
-            
-            var profString = profArray.toString();
-            var URLprofessorName = profString.replace(/,/g,'+');
-        }
-
-        if(exceptions[professorName]) {
-
-            var rating = findRatings(exceptions[professorName], professorName);
-            return rating;
-        } else {
-            rmpsearch = 'http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=The+Ohio+State+University&schoolID=724&query=PROFESSORNAME';
-            rmpsearch = rmpsearch.replace('PROFESSORNAME', URLprofessorName);
-        }
-
-        var rating = getProfessorExtension(rmpsearch, professorName);
-        return rating;
-}
-
-getProfessorExtension = function(searchPageURL, professorName){
-
-    var xmlRequestInfo = {
-        method: 'GET',
-        action: 'xhttp',
-        url: searchPageURL,
-        professorName: professorName
-    };
-
-    chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
-        var responseXML, professorName, ratings;
-        try {
-
-            responseXML = data.responseXML;
-            professorName = data.professorName;
-
-            //Find the numerical extension that leads to the specific professor's RMP page.
-            var professorURLExtension = $(responseXML).find('.listing:first').find('a:first').attr('href');
-
-            //Check to make sure a result was found
-            if (typeof professorURLExtension === 'undefined'){
-                var rating = "N/A";
-                return rating;
-                //updateRMPinfo('?','?', professorName);//update RMP cells with empty information
-            } else {
-                var professorPageURL = 'http://www.ratemyprofessors.com' + professorURLExtension;
-                rating = findRatings(professorPageURL, professorName);
-                return rating;
-            }
-        }
-        catch(err) {
-            // updateRMPinfo('?', '?', professorName);//update RMP cells with empty information
-            console.log('unable to find info')
-        }       
-    });
-}
-
-findRatings = function(professorPageURL, professorName){
-    var xmlRequestInfo = {
-        method: 'GET',
-        action: 'xhttp',
-        url: professorPageURL,
-        professorName: professorName
-    };
-
-    chrome.runtime.sendMessage(xmlRequestInfo, function(data) {
-
-        var rating = {
-            overall: -1,
-            // helpfulness: -1,
-            // clarity: -1,
-            //mostRecent: -1
-        };
-        var professorName, professorPageURL, responseXML;
-        try {
-
-            professorName = data.professorName;
-            professorPageURL = data.url;
-            responseXML = data.responseXML;
-
-            //Find the numerical extension that leads to the specific professor's RMP page.
-            rating.overall = $(responseXML).find('.grade').html();
-            //rating.helpfulness = $(responseXML).find('.rating:eq(0)').html();
-            // rating.clarity = $(responseXML).find('.rating:eq(1)').html();
-
-            //rating.difficulty = $(responseXML).find(":contains('Level of Difficulty'").find('.grade').html();              
-
-            // rating.mostRecent = $(responseXML).find('.rating:eq(1)').html();
-
-            //document.write(responseXML);
-
-            //Check to make sure a result was found
-            if (parseInt(rating.overall) > 5 || parseInt(rating.overall) <= 0 || isNaN(rating.overall)){
-                rating = '?';
-            }
-
-        }
-        catch(err) {
-            rating = '?';
-            return ratings;
-        }
-
-        //updateRMPinfo(professorPageURL, rating, professorName);
-
-        // return professor rating
-        return rating;
-    });
-}
-
-
 swapArrayElements = function(a, x, y) {
   if (a.length === 1) return a;
   a.splice(y, 1, a.splice(x, 1, a[y])[0]);
   return a;
 };
 
+// Need MutationObserver to watch for changes in the number of results on 'results' page. When triggered,
+// we modify page to include a "show rating" button for each professor, allow user to view RateMyProfessors.com
+// ratings
+//
 // select the target node
 var target = document.querySelector('.result-count');    
 var blobs = [];
@@ -408,22 +280,10 @@ var observer = new MutationObserver(function(mutations) {
     for(var i=0; i<blogLength; i++) {
         console.log(blobs[i]);
     }
-    createRMPHeader();
+    main();
 
 });
 
-getFinalResult = function(mutant) {
-    var tmp = mutant.target.innerText.split(" ");
-    var results = parseInt(tmp[0], 10);
-    return results;
-}
- 
-// function getResultsCount(target) {
-//     var temp = target.innerText.split(" ");
-//     // var results = parseInt(temp[0], 10);
-//     alert(temp[0].toString());
-// }
-// configuration of the observer:
 var options = { 'attributes': true, 'childList': true, 'characterData': true, 'subtree': true }
  
 // pass in the target node, as well as the observer options
